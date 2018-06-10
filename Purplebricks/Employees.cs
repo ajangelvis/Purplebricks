b5faa1c127637c97945c242738d3964347ed3a03
selenium selenium <seltesting@outlook.com>
2018-06-10 17:31:21 +0100

    bbb

---
+++ Purplebricks/Employees.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purplebricks
+{
+    class Employees
+    {
+        public  String ID { get; set; }
+
+        public String Name { get; set; }
+
+        public List<int> _Salary { get; set; }
+
+        public Employees(String id, String name, List<int> salary)
+        {
+            this.ID = id;
+            this.Name = name;
+            this._Salary = salary;
+        }
+
+
+        public static int SumEmployeeSalary(List<int> emplsalary)
+        {
+            int sum = 0;
+
+            foreach (int salary in emplsalary)
+            {
+                sum = sum + salary;
+            }
+
+            return sum;
+        }
+
+
+        public static int TotalSumEmployeeSalary(List<List<int>> emplsalary, int index)
+        {
+            int totalsum = 0;
+
+            foreach (List<int> salary in emplsalary)
+            {
+                totalsum = totalsum + salary[index];
+            }
+
+            return totalsum;
+        }
+
+
+
+
+    }
+}
---
+++ Purplebricks/NewEmployees.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purplebricks
+{
+    class NewEmployees
+    {
+        
+
+        public static void GetEmployeeMonthlySalaries()
+        {
+            List<Employees> employee = new List<Employees>();
+           
+
+            List<int> james_salary = new List<int> { 1500,1700,3000};
+
+            List<int> shawn_salary = new List<int> { 2100, 2200, 4000 };
+
+            List<int> adrian_salary = new List<int> { 4500, 1100, 1400 };
+
+            employee.Add(new Employees("ID001","James",james_salary));
+
+            employee.Add(new Employees("ID002", "shawn", shawn_salary));
+
+            employee.Add(new Employees("ID003", "adrian", adrian_salary));
+
+            List<List<int>> allsalary = new List<List<int>> { james_salary, shawn_salary, adrian_salary };
+
+            
+            var jamessalary = from total in james_salary where total > 0 select total;
+            
+            var shawnsalary = from total in shawn_salary where total > 0 select total;
+
+            var adriansalary = from total in adrian_salary where total > 0 select total;
+
+            var querry = allsalary.Select(i => i.ToArray());
+
+
+            foreach (var amt in querry )
+            {
+                Console.WriteLine(amt);
+                Console.WriteLine();
+
+            }
+
+
+            /*
+            foreach (var amt in jamessalary )
+            {
+                Console.WriteLine(amt);
+                Console.WriteLine( );
+
+            }
+
+            foreach (var amt in shawnsalary)
+            {
+                Console.WriteLine(amt);
+            }
+
+            foreach (var amt in adriansalary)
+            {
+                Console.WriteLine(amt);
+            }
+            */
+
+            Console.ReadLine();
+
+        }
+
+
+       
+    }
+}
